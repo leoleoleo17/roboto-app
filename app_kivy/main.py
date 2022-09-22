a16af262e4d2c3bc48337fcbe45ba0ec00d2a4e7
Sofia Duarte <sofia.duarte@urosario.edu.co>
2022-09-22 16:43:03 -0500

    Add existing file

---
+++ app_kivy/main.py
@@ -0,0 +1,27 @@
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from kivy.core.window import Window
+Window.size=(270,530)
+
+class Ui(ScreenManager):
+    message = ''
+
+    def textInput(self, widget):
+        if widget.text!='':
+            self.message = widget.text
+            print(self.message)
+            self.ids.mensaje.helper_text = 'Escribe un mensaje'
+            self.ids.mensaje.text = ''
+        else:
+            self.ids.mensaje.helper_text = '¡No envíes un mensaje vacío!'
+
+class MainApp(MDApp):
+    def build(self):
+        Builder.load_file('design.kv') #para la creación de los elementos de la app (widgets)
+
+        return Ui()
+
+
+if __name__ == "__main__":
+    MainApp().run()
