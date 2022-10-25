8024a82d209eef874980a3820012c8280aeab58d
Sofia Duarte <sofia.duarte@urosario.edu.co>
2022-10-24 21:08:20 -0500

    scroll en screen historia, cambio de colores

--- app_kivy/main.py
+++ app_kivy/main.py
@@ -8,11 +8,13 @@ class Ui(ScreenManager):
     message = ''
 
     def textInput(self, widget):
-        if widget.text!='':
+        if widget.text!='' and len(widget.text)<=160:
             self.message = widget.text
             print(self.message)
             self.ids.mensaje.helper_text = 'Escribe un mensaje'
             self.ids.mensaje.text = ''
+        elif len(widget.text)>160:
+            self.ids.mensaje.helper_text = '¡No envíes un mensaje largo!'
         else:
             self.ids.mensaje.helper_text = '¡No envíes un mensaje vacío!'
 
