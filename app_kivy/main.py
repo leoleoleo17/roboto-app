b3f029164c02098b54d48de33920f5b408eca7f8
Jddjraptor <92175427+Jddjraptor@users.noreply.github.com>
2022-11-01 16:23:59 -0500

    Update main.py

--- app_kivy/main.py
+++ app_kivy/main.py
@@ -2,11 +2,12 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
 from kivy.core.window import Window
+
 Window.size=(270,530)
 
 class Ui(ScreenManager):
     message = ''
-
+    
     def textInput(self, widget):
         if widget.text!='' and len(widget.text)<=160:
             self.message = widget.text
@@ -19,10 +20,10 @@ class Ui(ScreenManager):
             self.ids.mensaje.helper_text = '¡No envíes un mensaje vacío!'
 
 class MainApp(MDApp):
-    def build(self):
-        Builder.load_file('design.kv') #para la creación de los elementos de la app (widgets)
-        return Ui()
 
+    def build(self):
+        Builder.load_file('CornerLayout.py') #para la creación de los elementos de la app (widgets)
+        return Ui()
 
 if __name__ == "__main__":
     MainApp().run()
